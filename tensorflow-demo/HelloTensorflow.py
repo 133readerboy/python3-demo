b85a70496bcc42585ec022edcb04269103acc400
133readerboy <java_sxau@163.com>
2019-09-17 21:52:16 +0800

    编写入门程序

--- tensorflow-demo/HelloTensorflow.py
+++ tensorflow-demo/HelloTensorflow.py
@@ -7,3 +7,7 @@
  @File    : HelloTensorflow.py
  @Software: PyCharm
 '''
+import tensorflow as tf
+message = tf.constant('Welcome to the exciting world of Deep Neural Networks!')
+with tf.Session() as sess:
+    print(sess.run(message).decode())
