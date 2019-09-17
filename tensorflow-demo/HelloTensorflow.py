0e707454424c9ecac9bbd99fb087d1c4c1aa7aac
133readerboy <java_sxau@163.com>
2019-09-17 21:34:20 +0800

    删除内部python3-demo文件夹,新建helloworld

---
+++ tensorflow-demo/HelloTensorflow.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+'''
+ @Time    : 2019/9/17 21:33
+ @Author  : Spark
+ @Email   : java_sxau@163.com
+ @File    : HelloTensorflow.py
+ @Software: PyCharm
+'''
---
+++ tensorflow-demo/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+'''
+ @Time    : 2019/9/17 21:29
+ @Author  : Spark
+ @Email   : java_sxau@163.com
+ @File    : __init__.py.py
+ @Software: PyCharm
+'''
